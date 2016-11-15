f1a8e82b34f0177a0dbe46b92bf674e1111bcf51
celton-sw <celton-sw@BITFD>
2016-11-15 09:58:30 -0600

    Add project files.

---
+++ Goober/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * This program is a derivative calculator
+ * It takes simple x^2 derivatives
+ * 
+ */
+namespace Goober
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
